f593f1a8b6821480f3c77c2978ec4780735150ea
Aqueira <162311168+Aqueira@users.noreply.github.com>
2024-06-07 13:02:55 +0300

    Add files via upload

---
+++ src/custom_errors.rs
@@ -0,0 +1,48 @@
+use aqueiralibrary::errors::Errors;
+use log::error;
+use std::fmt;
+use std::num::ParseIntError;
+use std::string::FromUtf8Error;
+use std::time::SystemTimeError;
+use thiserror::Error;
+use tokio::sync::AcquireError;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Error: {0}")]
+    Error(String),
+    #[error("Join error: {0}")]
+    JoinError(#[from] tokio::task::JoinError),
+    #[error("Reqwest error: {0}")]
+    ReqwestError(#[from] reqwest::Error),
+    #[error("ToStr error: {0}")]
+    ToStrError(#[from] hyper::header::ToStrError),
+    #[error("Parse int error: {0}")]
+    ParseIntError(#[from] ParseIntError),
+    #[error("AcquireError: {0}")]
+    AcquireError(#[from] AcquireError),
+    #[error("std::io::Error: {0}")]
+    STDIOError(#[from] std::io::Error),
+    #[error("aqueiralibraryerror: {0}")]
+    AqueiraErrors(#[from] Errors),
+    #[error("SystemTimeError: {0}")]
+    SystemTimeError(#[from] SystemTimeError),
+    #[error("FromUtf8Error: {0}")]
+    FromUtf8Error(#[from] FromUtf8Error),
+    #[error("FromUtf8Error: {0}")]
+    BinCodeErrorKind(#[from] Box<bincode::ErrorKind>),
+}
+impl Error {
+    pub fn any(message: &str, err: impl fmt::Display) -> Self {
+        error!("{} -> {}", message, err);
+        Error::Error(format!("{} -> {}", message, err))
+    }
+    pub fn error(message: &str) -> Self {
+        Error::Error(format!("{}", message))
+    }
+}
+impl From<()> for Error {
+    fn from(_: ()) -> Self {
+        Error::Error("An unexpected error occurred".to_string())
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,98 @@
+mod custom_errors;
+
+use custom_errors::Error;
+use std::env::args;
+use std::fs;
+use std::path::Path;
+struct FileInformation {
+    bytes: u64,
+    lines: u32,
+    words: u32,
+}
+
+impl FileInformation {
+    fn new(bytes: u64, lines: u32, words: u32) -> Self {
+        FileInformation {
+            bytes,
+            lines,
+            words,
+        }
+    }
+
+    fn from(tuple: (u64, u32, u32)) -> Self {
+        let (bytes, lines, words) = tuple;
+        FileInformation::new(bytes, lines, words)
+    }
+}
+
+enum Commands {
+    GetBytes,
+    GetLines,
+    GetWords,
+    Empty,
+}
+
+fn main() -> Result<(), Error> {
+    let mut args = get_args()?;
+    let path = get_path(&mut args)?;
+    let command = parse_args_to_command(args);
+    let file_information = get_all_information(&path)
+        .map(|tuple| FileInformation::from(tuple))?;
+    print_command_result(&command, &file_information);
+    Ok(())
+}
+
+fn get_args() -> Result<Vec<String>, Error> {
+    let args: Vec<String> = args().skip(1).collect();
+    args_is_empty(&args)?;
+    Ok(args)
+}
+
+fn get_path(arg: &mut Vec<String>) -> Result<String, Error> {
+    if let Some(path) = arg.pop() {
+        return if Path::new(&path).exists() {
+            Ok(path)
+        } else {
+            Err(Error::error("Path does not exist!"))
+        };
+    }
+    Err(Error::error("Argument Path does not exist!"))
+}
+
+fn args_is_empty(args: &Vec<String>) -> Result<(), Error> {
+    if args.is_empty() {
+        return Err(Error::error("No arguments provided"));
+    }
+    Ok(())
+}
+
+fn parse_args_to_command(args: Vec<String>) -> Commands {
+    return match args.first().map(String::as_str) {
+        Some("-c") => Commands::GetBytes,
+        Some("-l") => Commands::GetLines,
+        Some("-w") => Commands::GetWords,
+        _ => Commands::Empty,
+    };
+}
+
+fn print_command_result(command: &Commands, file: &FileInformation) {
+    match command {
+        Commands::GetBytes => println!("Bytes in file: {}", file.bytes),
+        Commands::GetLines => println!("Lines in file: {}", file.lines),
+        Commands::GetWords => println!("Words in file: {}", file.words),
+        Commands::Empty => println!(
+            "All information:\nBytes: {}\nLines: {}\nWords: {}",
+            file.bytes, file.lines, file.words
+        ),
+    };
+}
+
+fn get_all_information(path: &str) -> Result<(u64, u32, u32), Error> {
+    let all_text = fs::read_to_string(path)?;
+
+    let count_bytes = fs::metadata(path)?.len();
+    let count_lines = all_text.lines().count() as u32;
+    let count_words = all_text.split_whitespace().count() as u32;
+
+    Ok((count_bytes, count_lines, count_words))
+}
