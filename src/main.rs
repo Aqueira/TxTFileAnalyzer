d65e0b174866014503ed3b73164ef5fe10c84741
Aqueira <162311168+Aqueira@users.noreply.github.com>
2024-06-10 11:46:23 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use log::{error, info};
 use std::env::args;
 use std::fs;
 use std::path::Path;
@@ -30,13 +31,19 @@ enum Commands {
     Empty,
 }
 
-fn main() -> Result<()> {
+fn main() -> Result<(), anyhow::Error> {
+    initial_logger();
     let mut args = get_args()?;
     let path = get_path(&mut args)?;
     let command = parse_args_to_command(&args);
     let file_information = get_all_information(&path).map(|tuple| FileInformation::from(tuple))?;
     print_command_result(&command, &file_information);
     Ok(())
+}
+
+fn initial_logger() {
+    std::env::set_var("RUST_LOG", "info");
+    env_logger::init();
 }
 
 fn get_args() -> Result<Vec<String>, anyhow::Error> {
@@ -50,14 +57,17 @@ fn get_path(arg: &mut Vec<String>) -> Result<String, anyhow::Error> {
         return if Path::new(&path).exists() {
             Ok(path)
         } else {
+            error!("Path does not exist!");
             Err(anyhow::anyhow!("Path does not exist!"))
         };
     }
-    Err(anyhow::anyhow!("Argument Path does not exist!"))
+    error!("Argument path does not exist!");
+    Err(anyhow::anyhow!("Argument path does not exist!"))
 }
 
 fn args_is_empty(args: &Vec<String>) -> Result<(), anyhow::Error> {
     if args.is_empty() {
+        error!("No arguments provided");
         return Err(anyhow::anyhow!("No arguments provided"));
     }
     Ok(())
@@ -74,10 +84,10 @@ fn parse_args_to_command(args: &Vec<String>) -> Commands {
 
 fn print_command_result(command: &Commands, file: &FileInformation) {
     match command {
-        Commands::GetBytes => println!("Bytes in file: {}", file.bytes),
-        Commands::GetLines => println!("Lines in file: {}", file.lines),
-        Commands::GetWords => println!("Words in file: {}", file.words),
-        Commands::Empty => println!(
+        Commands::GetBytes => info!("Bytes in file: {}", file.bytes),
+        Commands::GetLines => info!("Lines in file: {}", file.lines),
+        Commands::GetWords => info!("Words in file: {}", file.words),
+        Commands::Empty => info!(
             "All information:\nBytes: {}\nLines: {}\nWords: {}",
             file.bytes, file.lines, file.words
         ),
@@ -85,9 +95,17 @@ fn print_command_result(command: &Commands, file: &FileInformation) {
 }
 
 fn get_all_information(path: &str) -> Result<(u64, u32, u32), anyhow::Error> {
-    let all_text = fs::read_to_string(path)?;
+    let all_text = fs::read_to_string(path).map_err(|e| {
+        error!("Failed to read file content: {}", e);
+        e
+    })?;
 
-    let count_bytes = fs::metadata(path)?.len();
+    let count_bytes = fs::metadata(path)
+        .map_err(|e| {
+            error!("Failed to retrieve file metadata: {}", e);
+            e
+        })?
+        .len();
     let count_lines = all_text.lines().count() as u32;
     let count_words = all_text.split_whitespace().count() as u32;
 
